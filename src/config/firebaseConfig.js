fcb3e82bbb87a45c4a76103f4be4c46e5a5792a2
Sheikh Mirzaban <55521889+shaykhmirzaban@users.noreply.github.com>
2023-04-20 03:56:13 -0700

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,21 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBbtwq1wGLbU9Pp6NRWBtDQAat7iGaG6zs",
-  authDomain: "water-tanker-history.firebaseapp.com",
-  databaseURL: "https://water-tanker-history-default-rtdb.firebaseio.com",
-  projectId: "water-tanker-history",
-  storageBucket: "water-tanker-history.appspot.com",
-  messagingSenderId: "895351580710",
-  appId: "1:895351580710:web:a6335fd60e76e08fc89ee7",
-  measurementId: "G-WZZ1Y7BQ2Y",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
